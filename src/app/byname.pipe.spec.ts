546055dacfbfc331cd0845908500bd358e9ec370
willidaconceicao <willidaconceicao@gmail.com>
2019-11-28 13:18:11 +0100

    Deploying gh-pages

--- src/app/byname.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ByNamePipe } from './byname.pipe';
-
-describe('ByNamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ByNamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/byyear.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ByYearPipe } from './byyear.pipe';
-
-describe('ByYearPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ByYearPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
