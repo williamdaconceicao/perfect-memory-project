6cfea0f6092ddb88778a4fcadb456f7afd4f878f
William Da Conceicao <william.daconceicao@perfect-memory.com>
2020-02-12 18:46:51 +0100

    What have been done; 	Finish the update to scss 	Start the creation of media query for the responsivity

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import '@Stylesheet/Component/app/app.component.scss';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
